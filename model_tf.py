58bb7a1e74247d30204147896e00c4d6b05bc185
Hongbo Zhou <hzh@equinor.com>
2018-08-02 12:50:30 -0500

    use standard lrelu and batch normalization parameters

--- model_tf.py
+++ model_tf.py
@@ -85,6 +85,20 @@ class pix2pix:
             train=tf.group(update_losses, incr_global_step, gen_train),
         )
 
+# def lrelu(x, a):
+#     with tf.name_scope("lrelu"):
+#         # adding these together creates the leak part and linear part
+#         # then cancels them out by subtracting/adding an absolute value term
+#         # leak: a*x/2 - a*abs(x)/2
+#         # linear: x/2 + abs(x)/2
+
+#         # this block looks like it has 2 inputs on the graph unless we do this
+#         x = tf.identity(x)
+#         return (0.5 * (1 + a)) * x + (0.5 * (1 - a)) * tf.abs(x)
+
+# def batchnorm(inputs):
+#     return tf.layers.batch_normalization(inputs, axis=3, epsilon=1e-5, momentum=0.1, training=True, gamma_initializer=tf.random_normal_initializer(1.0, 0.02))
+
 def lrelu(x, a):
     with tf.name_scope("lrelu"):
         # adding these together creates the leak part and linear part
@@ -94,11 +108,10 @@ def lrelu(x, a):
 
         # this block looks like it has 2 inputs on the graph unless we do this
         x = tf.identity(x)
-        return (0.5 * (1 + a)) * x + (0.5 * (1 - a)) * tf.abs(x)
-
+        return tf.nn.leaky_relu(x)
 
 def batchnorm(inputs):
-    return tf.layers.batch_normalization(inputs, axis=3, epsilon=1e-5, momentum=0.1, training=True, gamma_initializer=tf.random_normal_initializer(1.0, 0.02))
+    return tf.layers.batch_normalization(inputs, axis=3, epsilon=1e-3, momentum=0.99, training=True, gamma_initializer=tf.random_normal_initializer(1.0, 0.02))
 
 def discrim_conv(batch_input, out_channels, stride):
     padded_input = tf.pad(batch_input, [[0, 0], [1, 1], [1, 1], [0, 0]], mode="CONSTANT")
